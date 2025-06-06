259f248a468b8d70b7f6885892f1ddb3ffb9eaad
arturocoh <huarca06@gmail.com>
2025-06-06 16:01:40 -0500

    actualizando

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,4 +10,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  adapter: vercel(),
 });
